84a8f2f29713e801a66bb1363802fffb53a8ec19
Nguyen Duc Hoa <your-email@example.com>
2025-04-16 18:17:14 +0700

    first commit

--- lib/constant/deviceIds.dart
+++
@@ -1,8 +0,0 @@
-class DevicesId {
-
-  static const int livingRoomLamp = 2;
-  static const int livingRoomAirConditioner = 3;
-  static const int livingRoomTV = 4;
-  static const int livingRoomFan = 5;
-  static const int wifi = 8;
-}
